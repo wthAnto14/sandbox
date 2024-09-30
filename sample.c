c4d2282ccf623b0ebc769993122c26312c72b98a
koraynilay <koray.fra@gmail.com>
2024-09-30 16:05:25 +0200

    Add square() call to sample.c and print info of what art it's going to draw

--- sample.c
+++ sample.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
 #include "src/asciiart.h"
 
 int main() {
+	printf("Printing amongus:\n");
 	draw("amongus");
+	printf("Printing square with default size:\n");
+	draw("square");
 	return 0;
 }
