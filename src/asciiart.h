62b1829f5f8c5dddfcc23b5bb9c93be88816d713
koraynilay <koray.fra@gmail.com>
2024-09-30 15:48:58 +0200

    Add amongus() draw function; Comment unimplemented quadrato() function

--- src/asciiart.h
+++ src/asciiart.h
@@ -1,7 +1,17 @@
+#include <stdio.h>
 #include <string.h>
+
+// draw functions prototypes
+void amongus();
 
 // takes a string as argument and runs
 // the appropriate drawing function
 void draw(const char* s) {
-	if(strcmp(s, "quadrato") == 0) quadrato();
+	//if(strcmp(s, "quadrato") == 0) quadrato(); // TODO: implement
+	if(strcmp(s, "amongus") == 0) amongus();
+}
+
+// draw functions
+void amongus() {
+	printf("ඞ\n");
 }
