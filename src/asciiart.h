07afda62cef519a1c5549ff2c76940fe906c1e2e
koraynilay <koray.fra@gmail.com>
2024-09-30 16:10:00 +0200

    Revert asciiart.h to cfc2fca (why did you put a while in draw()?? *cry*)

--- src/asciiart.h
+++ src/asciiart.h
@@ -3,20 +3,26 @@
 
 // draw functions prototypes
 void amongus();
+void square(int n);
 
 // takes a string as argument and runs
 // the appropriate drawing function
 void draw(const char* s) {
-	while(true){
-		//if(strcmp(s, "quadrato") == 0) quadrato(); // TODO: implement
-		if(strcmp(s, "amongus") == 0) amongus();
-		else if(strcmp(s, "amongus") == 0) square();
-	}
+	if(strcmp(s, "quadrato") == 0 || strcmp(s, "square") == 0) square(10);
+	if(strcmp(s, "amongus") == 0) amongus();
 }
-void square(){
-	printf("###\n###\n###\n");
-}
+
 // draw functions
+
 void amongus() {
 	printf("ඞ\n");
+}
+
+void square(int n) {
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			printf("#");
+		}
+		puts("");
+	}
 }
