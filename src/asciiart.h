cfc2fca876adeec3e44993b4ae21b816795b77f0
koraynilay <koray.fra@gmail.com>
2024-09-30 16:02:52 +0200

    Integrate square() function and remove misplaced "asciiart.c" and "asciiart.h" file

--- src/asciiart.h
+++ src/asciiart.h
@@ -3,15 +3,26 @@
 
 // draw functions prototypes
 void amongus();
+void square(int n);
 
 // takes a string as argument and runs
 // the appropriate drawing function
 void draw(const char* s) {
-	//if(strcmp(s, "quadrato") == 0) quadrato(); // TODO: implement
+	if(strcmp(s, "quadrato") == 0 || strcmp(s, "square") == 0) square(10);
 	if(strcmp(s, "amongus") == 0) amongus();
 }
 
 // draw functions
+
 void amongus() {
 	printf("ඞ\n");
+}
+
+void square(int n) {
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			printf("#");
+		}
+		puts("");
+	}
 }
