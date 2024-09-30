dbf5a95c345fac4b1250b3557eda70afee9fe5ec
LucaDallaVilla <luca.dallavilla@edu.unito.it>
2024-09-30 15:57:55 +0200

    rimozione file da cartella principale

--- asciiart.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// stampa un quadrato di lato n con i caratteri "#"
-void square(int n);
-
-
-// stampa la forma descritta dalla stringa in input
-void draw(char* shape);
