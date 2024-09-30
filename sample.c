66020132f48c92b9877a0f73eebc3bae4d6fc524
koraynilay <koray.fra@gmail.com>
2024-09-30 16:34:27 +0200

    Add arch logo to sample.c; Fix prints in sample.c; Fix no newline in circle() draw function

--- sample.c
+++ sample.c
@@ -2,11 +2,13 @@
 #include "src/asciiart.h"
 
 int main() {
-	printf("Printing amongus:\n");
+	printf("Printing default amount (10) of amongus:\n");
 	draw("amongus");
 	printf("Printing square with default size:\n");
 	draw("square");
-	printf("Printing circle with default size:\n square");
+	printf("Printing arch logo:\n");
+	draw("archlogo");
+	printf("Printing circle:\n");
 	draw("circle");
 	return 0;
 }
