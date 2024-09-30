ff9ec713d4a4a272a58db3a69ff7d460efd8387e
koraynilay <koray.fra@gmail.com>
2024-09-30 16:31:45 +0200

    Fix code; Add arch logo (TODO: Add circle)

--- src/asciiart.h
+++ src/asciiart.h
@@ -3,35 +3,54 @@
 
 // draw functions prototypes
 void amongus();
+void square(int n);
+void iusearchbtw();
 
 // takes a string as argument and runs
 // the appropriate drawing function
 void draw(const char* s) {
-	while(true){
-		if(strcmp(s, "amongus") == 0){
-			int i=0;
-			scanf("%d",&i);
-			amongus(i);
-		}
-		else if(strcmp(s, "square") == 0) square();
-		else if(strcmp(s, "circle") == 0) square();
-		else {
-			printf("Invalid input: %s\n", s);
-			exit(1);
-		}
-	}
+	if(strcmp(s, "quadrato") == 0 || strcmp(s, "square") == 0) square(10);
+	if(strcmp(s, "amongus") == 0) amongus(10);
+	if(strcmp(s, "archlogo") == 0 || strcmp(s, "iusearchbtw") == 0) iusearchbtw();
 }
-void square(){
-	printf("###\n###\n###\n");
-}
-void circle(){
-	printf("()");
-}
+
 // draw functions
+
 void amongus(int i) {
 	while(i>0){
 		printf("ඞ");
 		i--;
 	}
 	printf("\n");
+}
+
+void square(int n) {
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			printf("#");
+		}
+		puts("");
+	}
+}
+
+void iusearchbtw() {
+	printf("                  -`                 \n");
+	printf("                 .o+`                \n");
+	printf("                `ooo/                \n");
+	printf("               `+oooo:               \n");
+	printf("              `+oooooo:              \n");
+	printf("              -+oooooo+:             \n");
+	printf("            `/:-:++oooo+:            \n");
+	printf("           `/++++/+++++++:           \n");
+	printf("          `/++++++++++++++:          \n");
+	printf("         `/+++ooooooooooooo/`        \n");
+	printf("        ./ooosssso++osssssso+`       \n");
+	printf("       .oossssso-````/ossssss+`      \n");
+	printf("      -osssssso.      :ssssssso.     \n");
+	printf("     :osssssss/        osssso+++.    \n");
+	printf("    /ossssssss/        +ssssooo/-    \n");
+	printf("  `/ossssso+/:-        -:/+osssso+-  \n");
+	printf(" `+sso+:-`                 `.-/+oso: \n");
+	printf("`++:.                           `-/+/\n");
+	printf(".`                                 `/\n");
 }
