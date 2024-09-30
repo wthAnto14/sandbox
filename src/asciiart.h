b6a746d55c9cdb545d42459edbf1ac64707d5099
Skollwarynz <zante342@gmail.com>
2024-09-30 16:12:49 +0200

    Add files via upload

--- src/asciiart.h
+++ src/asciiart.h
@@ -3,26 +3,24 @@
 
 // draw functions prototypes
 void amongus();
-void square(int n);
 
 // takes a string as argument and runs
 // the appropriate drawing function
 void draw(const char* s) {
-	if(strcmp(s, "quadrato") == 0 || strcmp(s, "square") == 0) square(10);
-	if(strcmp(s, "amongus") == 0) amongus();
+	while(true){
+		//if(strcmp(s, "quadrato") == 0) quadrato(); // TODO: implement
+		if(strcmp(s, "amongus") == 0) amongus();
+		else if(strcmp(s, "square") == 0) square();
+		else if(strcmp(s, "circle") == 0) square();
+	}
 }
-
+void square(){
+	printf("###\n###\n###\n");
+}
+void circle(){
+	printf("()");
+}
 // draw functions
-
 void amongus() {
 	printf("ඞ\n");
-}
-
-void square(int n) {
-	for (int i=0; i<n; i++) {
-		for (int j=0; j<n; j++) {
-			printf("#");
-		}
-		puts("");
-	}
 }
